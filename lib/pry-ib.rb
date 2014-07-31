d50ac0595c9c4b2cbb17b07d9f5465d56684c8bf
David Kinsfather <david.kinsfather@gmail.com>
2014-07-31 07:45:58 -0700

    import for commands.

--- lib/pry-ib.rb
+++ lib/pry-ib.rb
@@ -3,6 +3,8 @@ require 'pry'
 require 'pry-ib/version'
 require 'pry-ib/commands'
 
-puts "Loading pry-ib 5"
+puts "Loading pry-ib 6"
 #Pry.config.prompt = proc { "IB> " }
 Pry.config.prompt = proc { |obj, nest_level, _| "IB: #{obj}(#{nest_level})> " }
+
+Pry.config.commands.import PryIb::Commands
