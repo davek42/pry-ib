720715a2302ffe9d4b02ad046b8f9c6ac1251de7
David Kinsfather <david.kinsfather@gmail.com>
2014-07-31 07:45:29 -0700

    First pry command: echo

--- lib/pry-ib/commands.rb
+++ lib/pry-ib/commands.rb
@@ -5,4 +5,15 @@ module PryIb
     def self.hello
       puts "Hello from pry-ib"
     end
+
+    Commands = Pry::CommandSet.new do
+        create_command "echo" do
+          description "DK - Echo the input: echo [ARGS]"
+
+          def process
+            output.puts "--->"
+            output.puts "ARGS: #{args.join(' ')}"
+          end
+        end
+    end
 end 
