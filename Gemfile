0f75d997491b55656f777317305b6584e761d8a8
David Kinsfather <david.kinsfather@gmail.com>
2014-09-01 07:30:45 -0700

    last attempt at getting to work with ib-ruby and activerecord conflict

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
+gem 'ib-ruby', :path => '/Users/davek/dev/dak/ib-ruby'
 # Specify your gem's dependencies in pry-ib.gemspec
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,30 @@ require_relative '../lib/pry-ib.rb'
 require 'pry/test/helper'
 
 
+#
+PryIb::Mongo::settings =
+    {"sessions"=>{
+      "default"=>{"database"=>"pryib-test",
+                  "hosts"=>["127.0.0.1:27017"],
+    }}}
+PryIb::Mongo::connect
 
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner[:mongoid].strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner[:mongoid].start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner[:mongoid].clean
+  end
+end
+
+
+#
 puts( %{
   -------------------------------------------------
   Ruby: #{RUBY_VERSION};
